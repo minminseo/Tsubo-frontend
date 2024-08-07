630a2a9d1dadecd60a6f891b4adb8c668153e4d0
Masahiro Yamamoto <cmvng00@gmail.com>
2024-08-08 01:09:07 +0900

    問い合わせ機能をGoogleフォームで代替

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,17 @@
-const Chat = () => {
-
+const Contact = () => {
     return (
+
         <div className="d-flex justify-content-center h-100 bg-success-subtle">
-                <h2 className="display-3">問い合わせのページ</h2>
+            <iframe
+                src="https://GoogleフォームのURL"
+                width="100%"
+                height="100%"
+                title="Google Forms Contact Form"
+                >
+                Loading…
+            </iframe>
         </div>
     );
 };
 
-export default Chat;
+export default Contact;
