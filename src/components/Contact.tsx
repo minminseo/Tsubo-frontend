f6b44b0546141c46fceed062c2f16f59518e5ceb
Masahiro Yamamoto <cmvng00@gmail.com>
2024-09-19 22:20:47 +0900

    各コンポーネントのコードに説明コメントを追加

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Contact = ({ lng }: { lng: string }) => {
     const formUrl =
-        lng === 'ko'
+        lng === 'ko' // もしlngが'ko'(true)なら、韓国語のGoogleフォームを表示
             ? 'https://docs.google.com/forms/d/e/1FAIpQLSfpwyzJylmwb-pUqHLqxci_qUzxRzgaljdDIkKaiUKfBLmhkw/viewform?usp=sf_link'
             : 'https://docs.google.com/forms/d/e/1FAIpQLSclnYJmhh38DlVQEns6FvWx9-6GlYN6Ht7eS-zzNPeVURwPbg/viewform?usp=sf_link';
 
     return (
-        <div className="d-flex justify-content-center h-100" style={{ backgroundColor: '#f5f8ef' }}>
+        <div className="d-flex justify-content-center h-100" style={{ backgroundColor: '#f5f8ef' }}> {/* #f5f8efはGoogleフォームの背景色と同じ */}ß
             <iframe
                 src={formUrl}
                 width="100%"
